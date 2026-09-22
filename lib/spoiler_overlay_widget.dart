fb14da3b9b939a0f74c01b16943548503c7c6ee2
Azamat Darmanov <darmanov.azamat@gmail.com>
2026-09-22 17:48:24 +0600

    fix: revert accidental spoiler overlay changes

--- lib/spoiler_overlay_widget.dart
+++ lib/spoiler_overlay_widget.dart
@@ -67,37 +67,35 @@ class _SpoilerOverlayState extends State<SpoilerOverlay>
 
   @override
   Widget build(BuildContext context) {
-    final child = ListenableBuilder(
-      listenable: _spoilerController,
-      child: widget.child,
-      builder: (context, child) {
-        return SpoilerRenderObjectWidget(
-          onAfterPaint: (canvas, size) => _onPaint(canvas, size),
-          onClipPath: (size) {
-            if (_spoilerController.isEnabled && !_spoilerController.isFading) {
-              return Path()..addRect(Offset.zero & size);
-            }
-
-            return _spoilerController.createClipPath(size);
-          },
-          enableOverlay: true,
-          imageFilter: _spoilerController.isEnabled ? widget.config.imageFilter : null,
-          child: child!,
-        );
-      },
-    );
-
-    if (widget.config.enableGestureReveal) {
-      return GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTapDown: (details) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTapDown: (details) {
+        if (widget.config.enableGestureReveal) {
           // Toggle the spoiler's visibility state through the controller.
           _spoilerController.toggle(details.localPosition);
-        },
-        child: child,
-      );
-    }
+        }
+      },
+      child: ListenableBuilder(
+        listenable: _spoilerController,
+        child: widget.child,
+        builder: (context, child) {
+          return SpoilerRenderObjectWidget(
+            onAfterPaint: (canvas, size) => _onPaint(canvas, size),
+            onClipPath: (size) {
+              if (_spoilerController.isEnabled &&
+                  !_spoilerController.isFading) {
+                return Path()..addRect(Offset.zero & size);
+              }
 
-    return child;
+              return _spoilerController.createClipPath(size);
+            },
+            enableOverlay: true,
+            imageFilter:
+                _spoilerController.isEnabled ? widget.config.imageFilter : null,
+            child: child!,
+          );
+        },
+      ),
+    );
   }
 }
